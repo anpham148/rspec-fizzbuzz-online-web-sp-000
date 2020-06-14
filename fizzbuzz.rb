eba731847cf393baacd71bdd25ce2c57cbba641e
An Pham <anpham148@gmail.com>
2020-06-14 10:42:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,15 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0
+     puts "Fizz"
      "Fizz"
     end
   elsif int % 5 == 0
+      puts "Buzz"
       "Buzz"
     end
   elsif int % 3 && int % 5 == 0
+      puts "Fizzbuzz"
       "Fizzbuzz"
     end
 
