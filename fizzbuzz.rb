dee305290301f2b670ae630f6a91ed762d9b3f89
An Pham <anpham148@gmail.com>
2020-06-14 10:59:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
-  if int % 3 == 0
-     "Fizz"
-    end
-  elsif int % 5 == 0
-      "Buzz"
-    end
-  elsif int % 3 && int % 5 == 0
-      "FizzBuzz"
-    end
-  else
-    nil
-  end
+
 end
