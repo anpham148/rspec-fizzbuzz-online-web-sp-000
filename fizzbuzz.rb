2196cf0dc64d3a70baf409821e7667fbe6a3f54f
An Pham <anpham148@gmail.com>
2020-06-14 10:34:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@
 def fizzbuzz (int)
   if int % 3 == 0
      "Fizz"
-  end
+    end
   elsif int % 5 == 0
       "Buzz"
     end
   elsif int % 3 && int % 5 == 0
       "Fizzbuzz"
-   end
+    end
   else
     nil
-   end
- 
+    end
+
 end
